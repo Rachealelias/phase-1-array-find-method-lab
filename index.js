5766aa943abdd28819a809d1bfd5c7ccef8cba04
Rachealelias <rachealsamuel708@gmail.com>
2021-07-07 22:30:04 -0400

    commit tree ec3603c31daa6f6f4b30cafd0651b297ea6212d8

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+ function superbowlWin(records){
+//     //console.log(record);
+     const solution = records.find( record => record.result === 'W');
+     return !!solution ? solution.year : undefined;
+//       //console.log(solution);
+
+ }
+
+// function superbowlWin(records){
+//     const solution = records.find(record => record.result === 'W');
+//     if (solution){
+//         return solution.year;
+//     }else {
+//         return undefined;
+//     }
 
